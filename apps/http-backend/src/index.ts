9c23b8ec3cb10463a5928b548613b9e2c3f7eaaf
Shivansh Tiwari <shivanshtiwari2014@gmail.com>
2026-03-17 12:01:02 +0530

    Task: added auth APIs

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import { JWT_SECRET } from '@repo/backend-common/config';
 import { prismaClient } from '@repo/db/client';
+import bcrypt from 'bcrypt';
 
 const app = express();
 
@@ -25,10 +26,12 @@ app.post('/signup', async (req, res) => {
       return res.status(400).json({ message: 'User already exists' });
     }
 
+    const encryptedPassword = await bcrypt.hash(password, 10);
+
     const newUser = await User.create({
       data: {
         email,
-        password,
+        password: encryptedPassword,
         firstName,
         lastName,
         image,
@@ -41,7 +44,36 @@ app.post('/signup', async (req, res) => {
   }
 });
 
-app.post('/signin', (req, res) => {});
+app.post('/signin', async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res
+        .status(400)
+        .json({ message: 'Please provide email and password' });
+    }
+
+    const existingUser = await User.findUnique({ where: { email } });
+
+    if (!existingUser) {
+      return res.status(400).json({ message: 'User does not exist' });
+    }
+
+    const isPasswordValid = await bcrypt.compare(
+      password,
+      existingUser.password,
+    );
+
+    if (!isPasswordValid) {
+      return res.status(400).json({ message: 'Invalid password' });
+    }
+
+    return res.status(200).json({ message: 'User signed in successfully' });
+  } catch (error) {
+    return res.status(400).json({ message: 'Failed to sign in' });
+  }
+});
 
 app.post('/room', (req, res) => {});
 
