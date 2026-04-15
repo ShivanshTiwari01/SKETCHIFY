343c2c2e93d9eca05ce4720eabd2a3f5bd4a7dad
Shivansh Tiwari <shivanshtiwari2014@gmail.com>
2026-04-15 15:31:28 +0530

    feat: added db package and setup prisma

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,4 +1,8 @@
 import express from 'express';
+import jwt, { JwtPayload } from 'jsonwebtoken';
+import { JWT_SECRET } from '@repo/backend-common/config';
+import { CreateUserSchema } from '@repo/common/types';
+import { prisma } from '@repo/db/prisma';
 
 const app = express();
 
@@ -6,7 +10,16 @@ const port = 4020;
 
 app.post('/api/signup', async (req, res) => {
   try {
-    const { username, password } = req.body;
+    const data = CreateUserSchema.safeParse(req.body);
+
+    if (!data) {
+      return res.status(400).json({
+        success: false,
+        message: 'Invalid Inputs',
+      });
+    }
+
+    const { username, password, name } = data;
 
     if (!username || !password) {
       return res.status(400).json({
